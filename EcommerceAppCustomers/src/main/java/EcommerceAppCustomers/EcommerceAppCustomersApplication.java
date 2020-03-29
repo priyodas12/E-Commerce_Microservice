3991751585d96bc5593227fb235eff2a42f63e23
Priyobrato Das <priyodas12@gmail.com>
2020-03-29 19:35:33 +0530

    Customer Microservice base package committed.

---
+++ EcommerceAppCustomers/src/main/java/EcommerceAppCustomers/EcommerceAppCustomersApplication.java
@@ -0,0 +1,15 @@
+package EcommerceAppCustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EcommerceAppCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppCustomersApplication.class, args);
+	}
+
+}
---
+++ EcommerceAppCustomers/src/test/java/EcommerceAppCustomers/EcommerceAppCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package EcommerceAppCustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
