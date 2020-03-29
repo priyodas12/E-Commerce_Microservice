02dbde5fbaaf3226de74dbbadee96308b898d3f1
Priyobrato Das <priyodas12@gmail.com>
2020-03-29 22:39:44 +0530

    added modified instance id

--- EcommerceAppCustomerManagement/src/main/java/EcommerceAppCustomerManagement/api/account/controller/AccountManagementController.java
+++ EcommerceAppCustomerManagement/src/main/java/EcommerceAppCustomerManagement/api/account/controller/AccountManagementController.java
@@ -1,5 +1,7 @@
 package EcommerceAppCustomerManagement.api.account.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/e-commercename/market/customer/account")
 public class AccountManagementController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status")
 	public String getAccountStatusO() {
-		return "200,OK";
+		return "200,OK from "+env.getProperty("local.server.port")+":"+env.getProperty("eureka.instance.instance-id");
 	}
 
 }
--- EcommerceAppCustomers/src/main/java/EcommerceAppCustomers/api/ui/controller/CustomerController.java
+++ EcommerceAppCustomers/src/main/java/EcommerceAppCustomers/api/ui/controller/CustomerController.java
@@ -1,5 +1,8 @@
 package EcommerceAppCustomers.api.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/e-commercename/market/customers")
 public class CustomerController {
+	
+	@Autowired
+	private Environment env;
+	
 	//status of this controller
 	@GetMapping("/status")
 	public String getStatus() {
-		return "200,OK";
+		return "200,OK from "+env.getProperty("local.server.port")+":"+env.getProperty("eureka.instance.instance-id");
 	}
 
 }
